de2d09babf103f3b59c9b5e2ede3547e8d390f93
Septian Rinaldi Rianggoro <septian.rianggoro@ist.id>
2023-10-16 12:37:38 +0700

    learn about expect and actual (more like interface in android development)

--- shared/src/commonMain/kotlin/io/github/septianrin/valkomp/Greeting.kt
+++ shared/src/commonMain/kotlin/io/github/septianrin/valkomp/Greeting.kt
@@ -1,9 +1,12 @@
 package io.github.septianrin.valkomp
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+        return "$firstWord, [$num] ${platform.name.reversed()}!"
     }
 }
---
+++ shared/src/iosSimulatorArm64Main/kotlin/io/github/septianrin/valkomp/num.kt
@@ -0,0 +1,3 @@
+package io.github.septianrin.valkomp
+
+actual val num: Int = 2
